a4440d6da45d386173213d6a8c8328ea64172227
lucashmorais <morais.lucas.h@gmail.com>
2015-03-26 01:48:20 -0300

    Resizing HD test to run in a few minutes on ordinary drives

--- jhd-tester/src/Constants.java
+++ jhd-tester/src/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 	static final int largeSizePageNumber = 512 * pagesInMB;
 	
 	//Input sets
-	static final InputSet small = new InputSet(131072, 64, 1, 36000, "small");
-	static final InputSet medium = new InputSet(393216, 192, 3, 108000, "medium");
-	static final InputSet large = new InputSet(655360, 320, 5, 180000, "large");	
+	static final InputSet small = new InputSet(16384, 4, 1, 10240, "small");
+	static final InputSet medium = new InputSet(49152, 12, 1, 10240, "medium");
+	static final InputSet large = new InputSet(81920, 20, 1, 10240, "large");	
 }
